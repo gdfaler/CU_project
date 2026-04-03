d46faaf8590c477f82002db7996211cfc5fe3472
Chil_Man <gdfaler@yandex.ru>
2026-04-03 14:53:12 +0300

    classes

---
+++ backend/models/User.py
@@ -0,0 +1,8 @@
+class User:
+    def __init__(self, username, roles):
+        self.username = username
+        self.roles = roles
+        self.lns = {}
+
+    def looking_for_team(self, desc):
+        pass
---
+++ backend/models/requests.py
@@ -0,0 +1,24 @@
+from typing import List
+
+from User import User
+
+
+class Request:
+    def __init__(self, req_id: int, user: User, desc):
+        self.req_id = req_id
+        self.user = user
+        self.desc = desc
+
+
+class TeamRequest(Request):
+    def __init__(self, req_id: int, user: User, desc: str, user_roles: List[str], user_lns: List[str]):
+        super().__init__(req_id, user, desc)
+        self.user_roles = user_roles
+        self.user_lns = user_lns
+
+
+class CoteamateRequest(Request):
+    def __init__(self, req_id: int, user: User, desc: str, nd_roles: List[str], nd_lns: List[str]):
+        super().__init__(req_id, user, desc)
+        self.user_roles = nd_roles
+        self.user_lns = nd_lns
